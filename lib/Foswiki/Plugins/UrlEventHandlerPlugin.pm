08e0f2b1e64607d25abf306c88a869a8592ec29f
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/UrlEventHandlerPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/UrlEventHandlerPlugin.pm
+++ lib/Foswiki/Plugins/UrlEventHandlerPlugin.pm
@@ -1,4 +1,4 @@
-# This script Copyright (c) 2008 Impressive.media 
+# This script Copyright (c) 2008 Impressive.media
 # and distributed under the GPL (see below)
 #
 # This program is free software; you can redistribute it and/or
@@ -9,7 +9,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 # Author: EugenMayer
 
@@ -39,7 +39,8 @@ $RELEASE = '0.1';
 
 # Short description of this plugin
 # One line description, is shown in the %FoswikiWEB%.TextFormattingRules topic:
-$SHORTDESCRIPTION = 'Lets you react on url parameters and e.g. show a message for the user or redirect him somewhere, show a dialog etc.';
+$SHORTDESCRIPTION =
+'Lets you react on url parameters and e.g. show a message for the user or redirect him somewhere, show a dialog etc.';
 
 # Name of this Plugin, only used in this module
 $pluginName = 'UrlEventHandlerPlugin';
@@ -50,72 +51,87 @@ my $jqPluginName = 'JQueryCompatibilityModePlugin';
 
 sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
-    my $pluginPubHome = Foswiki::Func::getPubUrlPath()."/System/$pluginName";
+    my $pluginPubHome = Foswiki::Func::getPubUrlPath() . "/System/$pluginName";
 
     my $query = Foswiki::Func::getCgiQuery();
-    
+
     # the CGI object is not set, we cant do anything.
-    return 1 if(!defined $query);
+    return 1 if ( !defined $query );
     my $params = {};
-    $params->{type} = $query->param("ueh_type") || "";
-    $params->{errorCode} = $query->param("ueh_errorcode") || "";
+    $params->{type}        = $query->param("ueh_type")        || "";
+    $params->{errorCode}   = $query->param("ueh_errorcode")   || "";
     $params->{confirmCode} = $query->param("ueh_confirmcode") || "";
     $params->{messageCode} = $query->param("ueh_messagecode") || "";
     $params->{messageCode} = $query->param("ueh_messagecode") || "";
-    $params->{dialogCode} = $query->param("ueh_dialogcode") || "";
-    
+    $params->{dialogCode}  = $query->param("ueh_dialogcode")  || "";
+
     # if type is not set or all codes are missing
-    if($params->{type} eq "" 
-        || ($params->{errorCode}.$params->{confirmCode}.$params->{messageCode}.$params->{dialogCode} eq "") 
-        || ($params->{type} eq "dialog" && $params->{dialogCode} eq "")
-      ) {
+    if (
+        $params->{type} eq ""
+        || (  $params->{errorCode}
+            . $params->{confirmCode}
+            . $params->{messageCode}
+            . $params->{dialogCode} eq "" )
+        || ( $params->{type} eq "dialog" && $params->{dialogCode} eq "" )
+      )
+    {
+
         #nothing to do for us
         return 1;
     }
-        
-    switch($params->{type}) {
-        case 'alert'{
+
+    switch ( $params->{type} ) {
+        case 'alert' {
             _alertHandler($params);
         }
         case 'mdialog' {
+
             # TODO: not implemented yet
             #_messageDialogHandler($params);
         }
         case 'autodialog' {
             _dialogHandler($params);
-        }        
+        }
     }
 
     return 1;
 }
 
 sub _alertHandler {
+
     # TODO: if js is not enabled, add a div in the upper
-    my $hashParams = shift; 
-    my $message = Foswiki::Func::loadTemplate("ueh_alert",Foswiki::Func::getSkin());
+    my $hashParams = shift;
+    my $message =
+      Foswiki::Func::loadTemplate( "ueh_alert", Foswiki::Func::getSkin() );
     $message = Foswiki::Func::expandCommonVariables($message);
-    # remove all newlines, because alert cant handle them    
+
+    # remove all newlines, because alert cant handle them
     $message =~ s/\n//g;
     $message =~ s/\r//g;
-    my $output = "<script type='text/javascript'>\$j(document).ready( function () { alert('$message'); });</script>";      
-    Foswiki::Func::addToHEAD($jqPluginName."_alertmessage",$output);     
+    my $output =
+"<script type='text/javascript'>\$j(document).ready( function () { alert('$message'); });</script>";
+    Foswiki::Func::addToHEAD( $jqPluginName . "_alertmessage", $output );
 }
 
 sub _messageDialogHandler {
-    my $hashParams = shift; 
-    my $dialogContent = Foswiki::Func::loadTemplate("ueh_mdialog",Foswiki::Func::getSkin());
+    my $hashParams = shift;
+    my $dialogContent =
+      Foswiki::Func::loadTemplate( "ueh_mdialog", Foswiki::Func::getSkin() );
     my $dialog = "$dialogContent";
-    
-    #my $output = "<script type='text/javascript'>$alert</script>";      
-    #Foswiki::Func::addToHEAD($jqPluginName."_alertmessage",$output, "JQueryCompatibilityModePlugin_jqui.dialog");     
+
+#my $output = "<script type='text/javascript'>$alert</script>";
+#Foswiki::Func::addToHEAD($jqPluginName."_alertmessage",$output, "JQueryCompatibilityModePlugin_jqui.dialog");
 }
 
 sub _dialogHandler {
-    my $hashParams = shift;      
-    my $fetchurl = Foswiki::Func::loadTemplate("ueh_autodialog",Foswiki::Func::getSkin());
-    $fetchurl =Foswiki::Func::expandCommonVariables($fetchurl);
+    my $hashParams = shift;
+    my $fetchurl =
+      Foswiki::Func::loadTemplate( "ueh_autodialog", Foswiki::Func::getSkin() );
+    $fetchurl = Foswiki::Func::expandCommonVariables($fetchurl);
     $fetchurl =~ s/\n//g;
     $fetchurl =~ s/\r//g;
-    my $output = "<script type='text/javascript'>\$j(document).ready( function () { show_dialog('#defaultDialog','$fetchurl','Dialog',true,600,600); });</script>";
-    Foswiki::Func::addToHEAD($jqPluginName."_autodialog",$output,$jqPluginName."_foswiki.dialogAPI" );    
+    my $output =
+"<script type='text/javascript'>\$j(document).ready( function () { show_dialog('#defaultDialog','$fetchurl','Dialog',true,600,600); });</script>";
+    Foswiki::Func::addToHEAD( $jqPluginName . "_autodialog",
+        $output, $jqPluginName . "_foswiki.dialogAPI" );
 }
